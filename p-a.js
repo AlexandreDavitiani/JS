d9108ba5d2c65af337092b728e851ed83d92c028
AlexandreDavitiani <135044890+AlexandreDavitiani@users.noreply.github.com>
2023-11-29 13:29:50 +0400

    Add files via upload

---
+++ p-a.js
@@ -0,0 +1,79 @@
+
+peremenii
+var num_1 = 21 ;
+var num_2 = 21;
+console.log("Price-first " + (num_1 + num_2) );
+
+var num_3 = 30;
+var num_4 = 29;
+console.log("Price-second "  + (num_3 - num_4) );
+
+var css1 = -20;
+var css2 = -2;
+console.log("Price-third "  + (css1 - css2)   )
+
+console.log("Math " + Math.PI);
+console.log("Math " + Math.E);
+console.log("Math " + Math.min(10,11,12,13,14,15,-1));
+
+var number = 15;
+if (number > 5) {
+  console.log("WELL")
+}
+onclick="document.getElementById('demo').innerHTML = Date()">
+Click me to display Date and Time.
+
+
+
+
+
+
+
+
+var andro = 1;
+var alex = 1;
+var arman = 1;
+if ( arman== 14 || alex==17 || andro==17 ) {
+console.log(true);
+} else {
+console.log(false)
+}
+
+
+
+
+onclick="document.getElementById('button').innerHTML = Date()"
+
+
+
+massivi//
+var arr = [5,true ,"stroka" , 10,19,-100];
+arr[1]="word";
+console.log(arr.length);
+
+
+var matrix = [
+  [4,6,8] , [2,3,"see"] , [10,19,-100]
+];
+
+matrix[1][2]=90;
+console.log(matrix);
+
+
+var test = 47;
+var test1 = 17;
+var test2 = 100;
+var test3 = 190;
+
+console.log( "True " + ( test1  + test + test2 + test3 ) );
+
+
+let x = 5;
+let y = 10;
+console.log("True " (  x + y)) ;
+
+
+
+var 98 = 155;
+var 99 = 145;
+console.log("TRUE" + ( 98 + 99 ));
